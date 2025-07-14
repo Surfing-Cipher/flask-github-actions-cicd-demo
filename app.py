208c9aa7d401fdc642fdbbf25e8a55bc4411a08e
Abdullah <abdullahzulfiqar068@gmail.com>
2025-07-15 01:52:52 +0500

    Update message

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from GitHub Actions CI/CD!"
+    return "CI/CD is working!"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
